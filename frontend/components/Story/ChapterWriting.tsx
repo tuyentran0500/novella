9808abf8e6496e75b8f30adaee0333da1e54a9dc
Tuyen Tran <s1f102000051@iniad.org>
2023-07-12 11:15:23 +0900

    Add editing editor page

--- frontend/components/Story/ChapterWriting.tsx
+++ frontend/components/Story/ChapterWriting.tsx
@@ -1,11 +1,20 @@
 import { useStoryContext } from "@/context/Story";
-import { Button, Card, CardActions, CardContent, CardMedia, CircularProgress, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardMedia } from "@mui/material";
 import React, { useState } from "react";
 import EditChapterWriting from "./EditChapterWriting";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import { getintialContent } from "@/helper/editor";
 const ChapterWriting = (): JSX.Element => {
-    const {selectedChapter, fetchSelectedChapterStatus, generateChapterContent, saveCurrentChapterContent} = useStoryContext();
+    const {selectedChapter, generateChapterContent, saveCurrentChapterContent} = useStoryContext();
     const [isEditing, setisEditing] = useState<boolean>(false)
-    if (isEditing) return <EditChapterWriting/>
+    const editor = useBlockNote({ initialContent: getintialContent(selectedChapter), editable: false})
+    const onEdit = () => {
+        setisEditing(prev => !prev);
+        if (editor){
+            editor.isEditable = !editor.isEditable;
+        }
+    }
+    if (isEditing) return <EditChapterWriting onEdit={onEdit} editor={editor}/>
     return (
         <Card className="m-5 w-full flex flex-col overflow-y-auto h-full">
             <CardMedia
@@ -13,19 +22,11 @@ const ChapterWriting = (): JSX.Element => {
                 image="https://images.unsplash.com/photo-1519791883288-dc8bd696e667?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
                 title="green iguana"
             />
-                <CardContent className="grow">
-                    {fetchSelectedChapterStatus === 'loading' && <CircularProgress/>}
-                    <Typography gutterBottom variant="h5" component="div">
-                    {selectedChapter.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {selectedChapter.content}
-                    </Typography>
-                </CardContent>
+            <BlockNoteView  editor={editor}/>
 
             <CardActions>
                 <Button size="small" onClick={generateChapterContent}>Generate</Button>
-                <Button size="small" onClick={() => setisEditing(prev => !prev)}>Edit</Button>
+                <Button size="small" onClick={onEdit}>Edit</Button>
                 <Button size="small" onClick={saveCurrentChapterContent}>Save</Button>
             </CardActions>
         </Card>
